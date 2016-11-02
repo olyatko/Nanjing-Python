d8737a317913dcec33244ddcb82824e367afaa86
Olga <charminola@yahoo.com>
2016-11-02 20:43:13 +0200

    Create w1quiz.py

---
+++ w1quiz.py
@@ -0,0 +1,12 @@
+# Nanjing University Python
+# 1 - T +
+# 2 - 9 < 1 and 10 < 9 or 2 > 1 INCORRECT!!!
+# 3 - print 'hello \
+#        world' INCORRECT!!!
+# 4 print math.pi NOT CHECKED!!!
+# 5 1//2 +
+# 6 Python是一种动态弱类型语言，同一个变量名，在不同位置可以被赋予不同的类型与数值。Python 赋值时大小写不敏感。(2 CORRECT ANSWERS) +
+# 7 函数int(object)将字符串和数字转换为整型。函数raw_input(propmt)获取用户输入，结果被看作是原始字符串。abs(num)返回值的绝对值。(3 CORRECT ANSWERS)+
+# 8 F +
+# 9 用import导入了模块之后，可以按照“模块.函数”的格式使用这个模块的函数。INCORRECT!!!
+# 10 AS, FROM - NOT CHECKED!!!
